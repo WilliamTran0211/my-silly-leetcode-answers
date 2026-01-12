118317049891afeb5783fbc5ca17beca47d964fb
William Tran <tiencuongtran.0211@gmail.com>
2026-01-12 09:00:39 +0700

    1266: Geometry pretending to be easy.

---
+++ 1266_min_time_visiting_all_points.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+
+        time = 0
+
+        for i in range(1, len(points)):
+            first = points[i - 1]
+            x, y = first[0], first[1]
+            curr_x, curr_y = points[i][0], points[i][1]
+            dx = abs(curr_x - x)
+            dy = abs(curr_y - y)
+            time += max(dx, dy)
+
+        return time
