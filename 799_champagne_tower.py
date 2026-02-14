ffd8e11e593dc3dd228f29f61609ba82b7131e76
William Tran <tiencuongtran.0211@gmail.com>
2026-02-14 14:04:10 +0700

    799: No more champagne and the fireworks are through. Here we are, me and you with Leetcode 799!

---
+++ 799_champagne_tower.py
@@ -0,0 +1,13 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        dp = [[0.0] * (query_row + 2) for _ in range(query_row + 2)]
+        dp[0][0] = poured
+
+        for i in range(query_row + 1):
+            for j in range(i + 1):
+                if dp[i][j] > 1:
+                    overflow = (dp[i][j] - 1) / 2
+                    dp[i + 1][j] += overflow
+                    dp[i + 1][j + 1] += overflow
+
+        return min(1.0, dp[query_row][query_glass])
