dba4decfab3da80f040c81e9e2ffe76d8ddaba3e
William Tran <tiencuongtran.0211@gmail.com>
2026-04-15 11:15:23 +0700

    2840: Same idea with 2839, scaled up to confuse me

---
+++ 2840_check_str_can_be_equal_operations_2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even1 = [s1[i] for i in range(0, len(s1), 2)]
+        odd1 = [s1[i] for i in range(1, len(s1), 2)]
+
+        even2 = [s2[i] for i in range(0, len(s2), 2)]
+        odd2 = [s2[i] for i in range(1, len(s2), 2)]
+
+        return sorted(even1) == sorted(even2) and sorted(odd1) == sorted(odd2)
