d1d020ce391a5dd1f7467babca1df176497c4e4d
William Tran <tiencuongtran.0211@gmail.com>
2026-04-18 11:34:37 +0700

    3740, 3741: Medium version just has better marketing.

---
+++ 3741_min_distance_between_three_equal_ele_2.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return -1
+
+        # abs(i - j) + abs(j - k) + abs(k - i) = (j-i) + (k-j) + (k-i) = j - i + j - k + k - i = 2k - 2i = 2(k-i)
+
+        his = defaultdict(list)
+
+        for i, n in enumerate(nums):
+            his[n].append(i)
+
+        ans = float("inf")
+
+        for key, val in his.items():
+            if len(val) >= 3:
+                for i in range(len(val) - 2):
+                    cal = 2 * (val[i + 2] - val[i])
+                    ans = min(ans, cal)
+        return ans if ans != float("inf") else -1
